abbf1e234ab3a44480333bb545822e697cfa95ba
Andrey Lemin <anlemin@mail.ru>
2018-08-05 17:08:16 +0300

    Add objects to rig prefab

--- Assets/PseudoCardboard/VrCamera.cs
+++ Assets/PseudoCardboard/VrCamera.cs
@@ -1,21 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace Assets.PseudoCardboard
 {
 	[RequireComponent(typeof(Camera))]
 	public class VrCamera : MonoBehaviour
 	{
+		private Camera _centralCam;
+		private Camera _leftEyeCam;
+		private Camera _rightEyeCam;
+
+		void Awake()
+		{
+			_centralCam = GetComponent<Camera>();
+
+			_leftEyeCam = GetComponentsInChildren<Camera>().First(cam => cam.stereoTargetEye == StereoTargetEyeMask.Left);
+			_rightEyeCam = GetComponentsInChildren<Camera>().First(cam => cam.stereoTargetEye == StereoTargetEyeMask.Right);
+
+			_leftEyeCam.transform.localPosition = - 0.5f * Vector3.right * _centralCam.stereoSeparation;
+			_rightEyeCam.transform.localPosition = 0.5f * Vector3.right * _centralCam.stereoSeparation;
+
+			_leftEyeCam.transform.localRotation = Quaternion.identity;
+			_rightEyeCam.transform.localRotation = Quaternion.identity;
+		}
+
 		void Update()
 		{
-			Camera camera = GetComponent<Camera>();
-
-			Vector3 leftCamPos = camera.transform.position - camera.transform.right * camera.stereoSeparation;
-			Vector3 rightCamPos = camera.transform.position + camera.transform.right * camera.stereoSeparation;
-
-			Camera cam = new Camera();
-
-			Matrix4x4 leftMatrix = camera.GetStereoProjectionMatrix(Camera.StereoscopicEye.Left);
-			Matrix4x4 rightMatrix = camera.GetStereoProjectionMatrix(Camera.StereoscopicEye.Right);
 
 		}
 	}
