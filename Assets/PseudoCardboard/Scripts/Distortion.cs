af47683953c07b24cd3a094c1a2f973a8c753209
Andrey Lemin <anlemin@mail.ru>
2018-10-27 23:26:47 +0300

    Fix Newton methods

--- Assets/PseudoCardboard/Scripts/Distortion.cs
+++ Assets/PseudoCardboard/Scripts/Distortion.cs
@@ -13,61 +13,53 @@ namespace Assets.PseudoCardboard
 			DistortionK2 = distortionK2;
 		}
 
-        public float UndistortOld(float radius)
-        {
-            float r0 = radius * 0.9f;
-            float r1 = radius / 0.9f;
-            float r2;
-            float dr1 = radius - Distort(r1);
-            float dr0;
-            while (Mathf.Abs(r0 - r1) > 0.0001f)
-            {
-                dr0 = radius - Distort(r0);
-                r2 = r0 - dr0 * ((r0 - r1) / (dr0 - dr1));
-                r1 = r0;
-                r0 = r2;
-                dr1 = dr0;
-            }
-            return r0;
-        }
+		public float UndistortOld(float radius)
+		{
+			float r0 = radius * 0.9f;
+			float r1 = radius / 0.9f;
+			float r2;
+			float dr1 = radius - Distort(r1);
+			float dr0;
+			while (Mathf.Abs(r0 - r1) > 0.0001f)
+			{
+				dr0 = radius - Distort(r0);
+				r2 = r0 - dr0 * ((r0 - r1) / (dr0 - dr1));
+				r1 = r0;
+				r0 = r2;
+				dr1 = dr0;
+			}
+			return r0;
+		}
 
-        public float Undistort(float r)
-        {
-            float rk = r;
-            float drk = DistortInner(rk) - r; // решаем уравнение f(x) = DistortInner(rk) - r = 0
+		public float Undistort(float r)
+		{
+			float rk = r;
+			float drk = Distort(rk) - r; // решаем уравнение f(x) = DistortInner(rk) - r = 0
 
-            int i = 0;
-            while (Mathf.Abs(r - drk) > 0.0001f && i < 100)
-            {
-                rk = rk - drk / DistortDerivative(rk);
-                drk = DistortInner(rk) - r;
-                ++i;
-            }
+			int i = 0;
+			while (Mathf.Abs(drk) > 0.0001f && i < 100)
+			{
+				rk = rk - drk / DistortDerivative(rk);
+				drk = Distort(rk) - r;
+				++i;
+			}
 
-            return rk;
-        }
+			return rk;
+		}
 
-        public float Distort(float r)
-        {
-            float dr = DistortInner(r);
-            float ur = Undistort(dr);
+		public float Distort(float r)
+		{
+			float r2 = r * r;
+			float r3 = r2 * r;
+			float r5 = r2 * r3;
+			return DistortionK2 * r5 + DistortionK1 * r3 + r;
+		}
 
-            return dr;
-        }
-
-        float DistortInner(float r)
-        {
-            float r2 = r * r;
-            float r3 = r2 * r;
-            float r5 = r2 * r3;
-            return DistortionK2 * r5 + DistortionK1 * r3 + r;
-        }
-
-        private float DistortDerivative(float r)
-        {
-            float r2 = r * r;
-            float r4 = r2 * r2;
-            return 5f + DistortionK2 * r4 + 3f * DistortionK1 * r2 + 1;
-        }
-    }
+		private float DistortDerivative(float r)
+		{
+			float r2 = r * r;
+			float r4 = r2 * r2;
+			return 5f + DistortionK2 * r4 + 3f * DistortionK1 * r2 + 1;
+		}
+	}
 }
